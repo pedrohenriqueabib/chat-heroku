ce121fcaeba2f833c9db2eb8f339e11203fde254
pedrohenriqueabib <pedrohenriqueabib@gmail.com>
2021-03-10 11:31:58 -0300

    tente

--- server.js
+++ server.js
@@ -25,5 +25,5 @@ io.on('connection', socket => {
     })
 });
 
-// server.listen(3000); //ouvira a porta 3000
-server.listen('https://chat-smoky-pi.vercel.app/');
+server.listen(4000); //ouvira a porta 3000
+// server.listen('https://chat-smoky-pi.vercel.app/');
